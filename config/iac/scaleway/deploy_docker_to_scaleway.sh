06ac6baf175c560a5d05dff1e2e2d5e7824b3216
Andreas Ek <andreas@elseif.se>
2019-02-13 17:07:56 +0100

    scaleway update

--- config/iac/scaleway/deploy_docker_to_scaleway.sh
+++ config/iac/scaleway/deploy_docker_to_scaleway.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export DOCKER_HOST=
-export DOCKER_TLS_VERIFY=
+export DOCKER_HOST=tcp://ip_address_scaleway:2375
+export DOCKER_TLS_VERIFY=0
 export DOCKER_CERT_PATH=
 
-docker-compose -f docker-compose.yaml -f scaleway.yaml up -d
+docker-compose -f ././../../../docker-compose.yaml -f scaleway.yaml up -d
--- config/iac/scaleway/scaleway.tf
+++ config/iac/scaleway/scaleway.tf
@@ -1,7 +1,7 @@
 provider "scaleway" {
   organization = "${var.org}"
-  token        = "${var.token}"
-  region       = "ams1"
+  token = "${var.token}"
+  region = "ams1"
 }
 
 resource "scaleway_ip" "ip" {
@@ -14,9 +14,9 @@ resource "scaleway_ip" "ip" {
 }
 
 resource "scaleway_server" "my_docker" {
-  name  = "my_docker"
+  name = "my_docker"
   image = "b33c14a6-3e2a-42f8-bebe-2f64a3ef6217"
-  type  = "START1-S"
+  type = "START1-S"
 }
 
 resource "scaleway_ssh_key" "my_docker" {
@@ -24,26 +24,41 @@ resource "scaleway_ssh_key" "my_docker" {
 }
 
 resource "scaleway_security_group" "http" {
-  name        = "http"
+  name = "http"
   description = "allow HTTP and HTTPS traffic"
 }
 
 resource "scaleway_security_group_rule" "http_accept" {
   security_group = "${scaleway_security_group.http.id}"
 
-  action    = "accept"
+  action = "accept"
   direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 80
+  ip_range = "0.0.0.0/0"
+  protocol = "TCP"
+  port = 80
 }
 
 resource "scaleway_security_group_rule" "https_accept" {
   security_group = "${scaleway_security_group.http.id}"
 
-  action    = "accept"
+  action = "accept"
   direction = "inbound"
-  ip_range  = "0.0.0.0/0"
-  protocol  = "TCP"
-  port      = 443
+  ip_range = "0.0.0.0/0"
+  protocol = "TCP"
+  port = 443
+}
+
+resource "scaleway_security_group" "docker" {
+  name = "docker"
+  description = "allow Docker deploy"
+}
+
+resource "scaleway_security_group_rule" "docker_accept" {
+  security_group = "${scaleway_security_group.docker.id}"
+
+  action = "accept"
+  direction = "inbound"
+  ip_range = "0.0.0.0/0"
+  protocol = "TCP"
+  port = 2375
 }
